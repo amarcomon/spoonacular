8b719abb463021c30ff315d888976833a88f93f4
amarco <antonio.marco@soprasteria.com>
2021-12-15 14:32:13 +0100

    feat(recipes): add call to api recipes

--- src/pages/recipes/Recipes.jsx
+++ src/pages/recipes/Recipes.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import {get} from "../../utils/httpClient";
+
 export function Recipes() {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        get("recipes/findByIngredients").then((randomRecipes) => {
+            setRecipes((prevRecipes) => prevRecipes.concat(randomRecipes));
+            console.log(recipes);
+        })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return(
         <h1>Recipes</h1>
     )
