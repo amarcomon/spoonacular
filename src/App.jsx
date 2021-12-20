a965748279acb4d92b70ccada6b775ce2bb61162
amarco <antonio.marco@soprasteria.com>
2021-12-20 15:46:24 +0100

    test(header): add test to component

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import { Recipes } from "./pages/recipes/Recipes";
 import { Header } from "./components/Header";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { faUtensilSpoon } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBook);
+
+library.add([faUtensilSpoon, faBook]);
 
 export const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route exact path="/" element={<Main />}></Route>
-        <Route exact path="/recipes" element={<Recipes />}></Route>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/recipes" element={<Recipes />} />
       </Routes>
     </Router>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,27 @@
+/* eslint-disable react/jsx-no-target-blank */
 import { Link } from "react-router-dom";
 import style from "./Header.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Header = () => {
+  const spoonacularUrl = "https://spoonacular.com/food-api/docs";
   return (
     <div className={style.headerContainer}>
       <Link to="/">
         <img className={style.imgLogo} src="./logo192.png" alt="React Logo" />
       </Link>
 
-      <Link className={style.link} to="/recipes">
-        <FontAwesomeIcon className={style.icon} icon="book" />
-        Recipes
-      </Link>
+      <div>
+        <Link className={style.link} to="/recipes">
+          <FontAwesomeIcon className={style.icon} icon="book" />
+          Recipes
+        </Link>
+
+        <a className={style.link} href={spoonacularUrl} target={'_blank'}>
+          <FontAwesomeIcon className={style.icon} icon="utensil-spoon" />
+          Spoonacular
+        </a>
+      </div>
     </div>
   );
 };
