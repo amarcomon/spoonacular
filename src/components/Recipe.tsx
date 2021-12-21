4f7088aa9314d7d66abb067c121a4d8f1077d6a3
amarco <antonio.marco@soprasteria.com>
2021-12-21 09:30:27 +0100

    feat(recipes): pasre html tag from summary

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -2,11 +2,15 @@ import { IRecipe } from "../interfaces/recipes.interface";
 import style from "./Recipe.module.css"
 
 export const Recipe = (recipe: IRecipe) => {
+  const parser = new DOMParser();
+  const parsedSummary = parser.parseFromString(recipe.summary, 'text/html');
+  console.log(parsedSummary.body.textContent);
+  
   return (
     <div className={style.container}>
       <h2 className={style.title}>{recipe.title}</h2>
       <img className={style.image} src={recipe.image} alt="" />
-      <p className={style.description}>{recipe.summary}</p>
+      <p className={style.description}>{parsedSummary.body.textContent}</p>
     </div>
   );
 };
