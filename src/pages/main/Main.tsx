0867c537d0188997a63be4a49c9425691087b837
amarco <antonio.marco@soprasteria.com>
2022-01-04 11:08:56 +0100

    feat(main): move recipes to main component

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import { Recipe } from "../../components/Recipe";
+import { IRecipe } from "../../interfaces/recipes.interface";
+import { getRandomRecipes } from "../../services/recipes.service";
+import style from "./Main.module.css";
+
 export const Main = () => {
-  return <h1>Main Component</h1>;
+  const recipesArray: Array<IRecipe> = [];
+  const [recipes, setRecipes] = useState(recipesArray);
+
+  useEffect(() => {
+    getRandomRecipes("recipes/random", 50).then((randomRecipes) => {
+      setRecipes(randomRecipes.recipes);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      <h1>Random recipes</h1>
+      <div className={style.recipeContainer}>
+        {recipes.map((recipe) => {
+          return <Recipe key={recipe.id} {...recipe} />;
+        })}
+      </div>
+    </div>
+  );
 };
